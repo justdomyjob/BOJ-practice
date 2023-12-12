d344b8652d3ac0eec3f8c7c895ced6d741956837
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2023-12-12 23:25:01 +0900

    [Silver II] Title: 나무 자르기, Time: 2260 ms, Memory: 144340 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,23 @@
+try : standard_input = open("input.txt", "r")  
+except:pass
+
+tree_n , tree_meter = map(int,input().split())
+tree_list = list(map(int,input().split()))
+
+left = 0
+right = max(tree_list)
+mid = (left+right)//2
+
+def get_tree(a):
+    sum = 0
+    for tree in tree_list:
+        if tree>= a:
+            sum+=(tree-a)
+    return sum
+while left+1 < right:
+    if get_tree(mid) >= tree_meter:
+        left = mid
+    else:
+        right = mid
+    mid = (left+right)//2
+print(mid)
