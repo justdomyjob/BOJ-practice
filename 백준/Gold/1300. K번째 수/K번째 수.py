5874d5f41c766c8e0f8304ee757add904f57e73a
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2023-12-13 13:24:17 +0900

    [Gold I] Title: K번째 수, Time: 688 ms, Memory: 33184 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -5,33 +5,21 @@ N =int(input())
 k =int(input())
 
 from bisect import bisect_left, bisect_right
-def nth(a): #a가 몇번째 수인지를 리턴
-    nsmall=0
-    nsame=0
+def nsmall(a): #ㄴㅇㄻㄴㄷㅇㄻ니아ㅓㅣ
+    smaller=0
     for i in range(1,N+1):
-        if a%i==0:
-            if a <= N*i :
-                nsame += 1
-                nsmall += (a//i -1)
-            else:  # a>N
-                nsame += 0 
-                nsmall += min(a//i,N)
-        else:
-            nsmall += min((a//i),N)
-    return (nsmall, nsame)
+        smaller += min((a//i),N)
+    return smaller
 
 left = 0
-right = min(10**9, N*N) + 1
+right = min(10**9, N*N)
 mid = (left+right)//2
 
-while left + 1 < right:
-    smaller = nth(mid)[0]
-    same = nth(mid)[1]
-    if smaller < k <=smaller +same:
-        break
-    elif k > smaller + same:
+while left +1 < right:
+    smaller = nsmall(mid)
+    if k > smaller: #mid는 더 커져야됨
         left = mid
     else : 
         right =mid
     mid = (right+left)//2
-print(mid)
+print(mid+1) 
