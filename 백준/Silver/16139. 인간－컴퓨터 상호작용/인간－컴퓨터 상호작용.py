9c3d93bc299de995c5e26c151b9ad6cd59410052
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2023-12-14 12:18:15 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 620 ms, Memory: 219012 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,8 +1,8 @@
 try : standard_input = open("input.txt", "r")  
 except:pass
-
-string = input()
-q = int(input())
+import sys
+string = sys.stdin.readline().rstrip()
+q = int(sys.stdin.readline().rstrip())
 sum= [[0] for _ in range(26)]
 for alphabet in string:
     for i in range(26):
@@ -11,6 +11,6 @@ for alphabet in string:
         else:
             sum[i].append(sum[i][-1])
 for _ in range(q):
-    a, i, j = input().split()
+    a, i, j = sys.stdin.readline().rstrip().split()
     i, j =int(i), int(j)
     print(sum[ord(a)-97][j+1]-sum[ord(a)-97][i])
