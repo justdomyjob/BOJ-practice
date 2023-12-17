0aabf693ba18b847625a2d7147f21bd27316d515
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2023-12-17 21:26:46 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1020 ms, Memory: 33012 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,33 @@
+try : standard_input = open("input.txt", "r")  
+except:pass
+
+dp={}
+def recur(a,b,c):
+    if (a,b,c) in dp:
+        return dp[(a,b,c)]
+    elif a <= 0 or b <= 0 or c <= 0:
+        dp[(a,b,c)]=1
+        return 1
+    elif a > 20 or b > 20 or c > 20 :
+        dp[(a,b,c)] = recur(20,20,20)
+        return dp[(a,b,c)]
+
+    elif a < b and b < c:
+        dp[(a,b,c-1)] = recur(a,b,c-1)
+        dp[(a,b-1,c-1)] = recur(a,b-1,c-1)
+        dp[(a,b-1,c)] = recur(a,b-1,c)
+        return dp[(a,b,c-1)]+dp[(a,b-1,c-1)]-dp[(a,b-1,c)]
+
+    else:
+        dp[(a-1,b,c)] = recur(a-1, b, c)
+        dp[(a-1,b-1,c)] = recur(a-1, b-1, c)
+        dp[(a-1,b,c-1)] = recur(a-1, b, c-1)
+        dp[(a-1,b-1,c-1)] = recur(a-1, b-1, c-1)
+        return dp[(a-1,b,c)] + dp[(a-1,b-1,c)] + dp[(a-1,b,c-1)] - dp[(a-1,b-1,c-1)]
+    
+while True:
+    a,b,c = map(int,input().split())
+    if a==-1 and b==-1 and c==-1 :
+        break
+    print(f"w({a}, {b}, {c}) = {recur(a,b,c)}")
+
