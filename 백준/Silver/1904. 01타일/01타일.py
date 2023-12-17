8dc46606c248bc1f8a2c02da3416f79ab4db4429
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2023-12-17 21:40:33 +0900

    [Silver III] Title: 01타일, Time: 360 ms, Memory: 70676 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,10 @@
+try : standard_input = open("input.txt", "r")  
+except:pass
+
+n = int(input())
+dp = [0] * 1000001
+dp[1] =1
+dp[2] =2
+for i in range(3,n+1):
+    dp[i] = dp[i-1]%15746 +dp[i-2]%15746
+print(dp[n]%15746)
