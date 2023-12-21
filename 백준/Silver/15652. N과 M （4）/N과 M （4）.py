46de0fe310619ed5a73cc89841d4f5fab2a436e0
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2023-12-21 20:52:10 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,21 @@
+try : standard_input = open("input.txt", "r")  
+except:pass
+
+n, m =map(int,input().split())
+
+s = []
+def dfs():
+    if len(s)==m:
+        print(" ".join(map(str,s)))
+    else:
+        for i in range(1,n+1):
+            if s:
+                if i >= s[-1]:
+                    s.append(i)
+                    dfs()
+                    s.pop()
+            else:
+                s.append(i)
+                dfs()
+                s.pop()
+dfs()
