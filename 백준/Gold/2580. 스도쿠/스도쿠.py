bb65e6fc51427604635e3f5a18ddc2462e5794ac
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2023-12-21 23:39:29 +0900

    [Gold IV] Title: 스도쿠, Time: 2916 ms, Memory: 134644 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,50 +1,33 @@
 import sys
-graph = []
-blank = []
 
-for i in range(9):
-    graph.append(list(map(int, sys.stdin.readline().rstrip().split())))
-
-for i in range(9):
-    for j in range(9):
-        if graph[i][j] == 0:
-            blank.append((i, j))
-
-def checkRow(x, a):
+graph = [list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(9)]
+zero_list = []
+for j in range(9):
     for i in range(9):
-        if a == graph[x][i]:
-            return False
-    return True
-
-def checkCol(y, a):
-    for i in range(9):
-        if a == graph[i][y]:
-            return False
-    return True
-
-def checkRect(x, y, a):
-    nx = x // 3 * 3
-    ny = y // 3 * 3
-    for i in range(3):
-        for j in range(3):
-            if a == graph[nx+i][ny+j]:
-                return False
-    return True
-
-
-def dfs(idx):
-    if idx == len(blank):
+        if graph[j][i]== 0:
+            zero_list.append((i,j))
+def dfs(n):
+    if n==len(zero_list):
         for i in range(9):
             print(*graph[i])
         exit(0)
-
-    for i in range(1, 10):
-        x = blank[idx][0]
-        y = blank[idx][1]
-
-        if checkRow(x, i) and checkCol(y, i) and checkRect(x, y, i):
-            graph[x][y] = i
-            dfs(idx+1)
-            graph[x][y] = 0
-
+    for i in range(1,10):
+        x,y = zero_list[n]
+        if can(x,y,i):
+            graph[y][x] = i
+            dfs(n+1)
+            graph[y][x] = 0
+def can(x,y,i):
+    for j in range(9):
+        if i == graph [j][x]:
+            return False
+    for j in range(9):
+        if i == graph [y][j]:
+            return False
+    a, b = (x//3) * 3, (y//3) * 3
+    for k in range(b,b+3):
+        for j in range(a,a+3):
+            if graph[k][j] == i:
+                return False
+    return True
 dfs(0)
