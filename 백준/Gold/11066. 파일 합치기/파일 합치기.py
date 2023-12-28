9056ee8570c789ab35c26356d323674b5e03a30e
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2023-12-29 00:26:41 +0900

    [Gold III] Title: 파일 합치기, Time: 1680 ms, Memory: 125796 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -1,38 +1,24 @@
-try : standard_input = open("input.txt", "r")  
-except:pass
+import sys
+try:
+    sys.stdin = open("input.txt", "r") 
+except:
+    pass
 
-def cost(left,right):
-    if dp[left][right]!=-1:
-        return dp[left][right]
-    if left > right:
-        print("error")
-        exit(0)
-    elif left == right:
-        return 0
-    else:
-        min_cost = MAX_COST
-        file_size = part_sum(left, right)
-        for i in range(left,right):
-            temp = cost(left,i) + cost(i+1,right)
-            if  temp < min_cost:
-                min_cost = temp
-        dp[left][right] = min_cost + file_size
-        return dp[left][right]
-
-def part_sum(left, right):
-    if left>=1:
-        file_size = dpsum[right]-dpsum[left-1]
-    else:
-        file_size = dpsum[right]
-    return file_size
-
-n = int(input())
-MAX_COST = 1000000000
-for _ in range(n):
-    K = int(input())
-    files = list(map(int,input().split()))
-    dp = [[-1 for _ in range(K)] for _ in range(K)]
-    dpsum=[0 for _ in range(K)]
-    for i in range(K):
-        dpsum[i]=dpsum[i-1] + files[i]
-    print(cost(0,K-1))
+T = int(input())
+for _ in range(T):
+    k = int(input())
+    sumdp = [[0 for _ in range(k) ] for _ in range(k)]
+    dp = [[0 for _ in range(k) ] for _ in range(k)]
+    file = list(map(int, input().split()))
+    for i in range(k):
+        sumdp[i][i] = file[i]
+    for i in range(1,k):
+        for j in range(k-i):
+            min = 1000000000
+            sumdp[j][i+j] = file[j] + sumdp[j+1][i+j]
+            for l in range(j,i+j):
+                temp = dp[j][l]+dp[l+1][i+j]
+                if temp < min:
+                    min = temp
+            dp[j][i+j] = min + sumdp[j][i+j]
+    print(dp[0][k-1])
