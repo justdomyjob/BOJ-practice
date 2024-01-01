3864bf63908b7f2c7fc480ee061791e1bb0a4379
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-01-01 22:10:44 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 720 ms, Memory: 65124 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,29 +1,25 @@
 import sys
 from collections import deque
+sys.setrecursionlimit(10**6)
 
 N, M = map(int,sys.stdin.readline().rstrip().split())
 E = [[] for _ in range(N+1)]
 visited = [0 for _ in range(N+1)]
 visited[0] = 1
+
 for _ in range(M):
     u, v = map(int,sys.stdin.readline().rstrip().split())
     E[u].append(v)
     E[v].append(u)
 
-def bfs(R):
-    node = deque()
+def dfs(R):
     visited[R] = 1
-    node.append(R)
-    while node:
-        v = node.pop()
-        for u in E[v]:
-            if visited[u]==0:
-                visited[u]=1
-                node.append(u)
-                
+    for u in E[R]:
+        if visited[u]==0:
+            dfs(u)
 ans = 0
 for i in range(1,N+1):
     if visited[i]==0:
-        bfs(i)
+        dfs(i)
         ans += 1
 print(ans)
