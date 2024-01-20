e852f47f972b9dc84b8554cdec213e2b9c9e828c
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-01-20 23:17:58 +0900

    [Gold IV] Title: 플로이드, Time: 620 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(input())
+m = int(input())
+INF = 10**9
+graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
+
+for i in range(1,n+1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a,b,c = map(int,sys.stdin.readline().rstrip().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            graph[i][j] = min(graph[i][k] + graph[k][j],graph[i][j])
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if graph[i][j] == INF:
+            print(0, end = " ")
+        else:
+            print(graph[i][j], end =" ")
+    print()
