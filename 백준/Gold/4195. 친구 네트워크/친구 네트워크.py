14b82cb2f9fd21f25bffe0ebd892f7ed4472a4e1
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-01-21 12:51:58 +0900

    [Gold II] Title: 친구 네트워크, Time: 300 ms, Memory: 61160 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -6,13 +6,19 @@ def find(parent, x):
         parent[x] = find(parent, parent[x])
     return parent[x]
 
-def union(parent,a,b):
+def union(count,parent,a,b):
     a = find(parent,a)
     b = find(parent,b)
     if a<b:
         parent[b] = a
-    else:
+        count[a] += count[b]
+        return count[a]
+    elif a>b:
         parent[a] = b
+        count[b] += count[a]
+        return count[b]
+    else:
+        return count[a]
 
 test = int(input())
 for _ in range(test):
@@ -21,20 +27,11 @@ for _ in range(test):
     count = {}
     for _ in range(n):
         a, b= sys.stdin.readline().rstrip().split()
-        parent[a] = parent.get(a,a)
-        parent[b] = parent.get(b,b)
-        
-        root_a = find(parent,a)
-        root_b = find(parent,b)
-        
-        if root_a == root_b:
-            print(count[root_a])
-            continue
-        count[root_a] = count.get(root_a,1)
-        count[root_b] = count.get(root_b,1)
-        
-        union(parent,a,b)
-
-        new_root = find(parent,a)
-        count[new_root] = count[root_a] + count[root_b]
-        print(count[new_root])
+        if a not in parent:
+            parent[a] = a
+            count[a] =1
+        if b not in parent:
+            parent[b] = b
+            count[b] = 1
+        new_count = union(count,parent,a,b)
+        print(new_count)
