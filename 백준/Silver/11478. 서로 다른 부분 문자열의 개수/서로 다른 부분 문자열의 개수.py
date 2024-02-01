4224c10f8a274c4023a11717d98049c20313fda5
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-01 14:06:37 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 560 ms, Memory: 310628 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -1,18 +1,10 @@
-from collections import deque
-
-string = input()
-
-def count(n): #길이가 n인 부분문자열 개수
-    ret = 1
-    sub = []
-    for i in range(len(string)+1-n):
-        sub.append(string[i:i+n])
-    sub.sort()
-    for i in range(1,len(sub)):
-        if sub[i]!=sub[i-1]:
-            ret+=1
-    return ret
-sum = 0
-for i in range(1,len(string)+1):
-    sum += count(i)
-print(sum)
+# import sys
+#
+# input = sys.stdin.readline
+a = input()
+length = len(a)
+total = set()
+for l in range(1,length+1):
+    for i in range(length+1-l):
+        total.add(a[i:i+l])
+print(len(total))
