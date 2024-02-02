8f5067eee80c3927a0feafd07d7c7538bb0d7525
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-02 20:38:06 +0900

    [Gold IV] Title: 빙산, Time: 828 ms, Memory: 213700 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,25 +1,26 @@
-import sys
-sys.setrecursionlimit(10**4)
+from collections import deque
+
 r, c = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(r)]
-
-steps = [(1,0),(-1,0),(0,1),(0,-1)]
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 visited = [[0 for _ in range(c)] for _ in range(r)]
-
 minus = [[0 for _ in range(c)] for _ in range(r)]
 
-def dfs(x,y):
-    visited[x][y] = 1
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0<=nx< r and 0<=ny < c:
-            if graph[nx][ny]!=0 and visited[nx][ny] == 0:
-                visited[nx][ny] = 1
-                dfs(nx,ny)
-            elif graph[nx][ny]==0:
-                minus[x][y] +=1
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
+    while q:
+        x,y = q.pop()
+        visited[x][y] = 1
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx< r and 0<=ny < c:
+                if graph[nx][ny]!=0 and visited[nx][ny] == 0:
+                    visited[nx][ny] = 1
+                    q.append((nx,ny))
+                elif graph[nx][ny]==0:
+                    minus[x][y] +=1
 def melt():
     for i in range(r):
         for j in range(c):
@@ -29,14 +30,13 @@ def init():
     visited = [[0 for _ in range(c)] for _ in range(r)]
     minus = [[0 for _ in range(c)] for _ in range(r)]
 
-
 year = 0
 while True:
     num_component = 0
     for i in range(r):
         for j in range(c):
             if graph[i][j]!=0 and visited[i][j]==0:
-                dfs(i,j)
+                bfs(i,j)
                 num_component +=1
     melt()
     init()
