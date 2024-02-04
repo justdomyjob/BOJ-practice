6ecd09bc3b9503066d89a88a85a38b3376d1e74e
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-04 14:09:43 +0900

    [Gold III] Title: 치즈, Time: 1676 ms, Memory: 32604 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -15,37 +15,34 @@ def dfs_air(i,j):
             if visited[ni][nj] == -1:
                 dfs_air(ni,nj)
 
-def melt(i,j):
+def melt():
     global graph
     temp_graph = [array[:] for array in graph]
-    dfs_melt(i, j,temp_graph)
+    for i in range(n):
+        for j in range(m):
+            count_air = 0
+            for k in range(4):
+                ni = i + dy[k]
+                nj = j + dx[k]
+                if 0 <= ni < n and 0 <= nj < m and graph[ni][nj] == AIR:
+                    count_air += 1
+            if count_air >= 2:
+                temp_graph[i][j] = AIR
     graph = temp_graph
 
-def dfs_melt(i,j,temp_graph):
-    visited[i][j] = 1
-    count_air = 0
-    for k in range(4):
-        ni = i + dy[k]
-        nj = j + dx[k]
-        if 0<=ni<n and 0<=nj<m and graph[ni][nj] == CHEEZE:
-            if visited[ni][nj] == -1:
-                dfs_melt(ni,nj,temp_graph)
-        if 0 <= ni < n and 0 <= nj < m and graph[ni][nj] == AIR:
-            count_air+=1
-    if count_air>=2:
-        temp_graph[i][j] = BLANK
+def all_melt():
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j]==1:
+                return False
+    return True
 
 time = 0
 while True:
-    count = 0
     visited = [[-1] * m for _ in range(n)]
     dfs_air(0, 0)
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j]==CHEEZE and visited[i][j]==-1:
-                melt(i,j)
-                count+=1
-    if count==0:
-        break
+    melt()
     time += 1
+    if all_melt():
+        break
 print(time)
