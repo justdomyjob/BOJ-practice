84e13469adc8f21372022eedb41877ed839dd3a1
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-04 15:06:52 +0900

    [Silver II] Title: 참외밭, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -0,0 +1,12 @@
+n = int(input())
+edges = [list(map(int,input().split()))[1] for _ in range(6)]
+
+steps = [None, (0,1),(0,-1),(1,0),(-1,0)]
+edges1 = [edges[i] for i in range(6) if i%2==0]
+edges2 = [edges[i] for i in range(6) if i%2==1]
+m1 = max(edges1)
+m2 = max(edges2)
+i1 = (edges.index(m1) + 3)%6
+i2 = (edges.index(m2) + 3)%6
+
+print((m1*m2 - edges[i1]*edges[i2])*n)
