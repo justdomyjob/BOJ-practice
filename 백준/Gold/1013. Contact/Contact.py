96893d5569b7580e7429182a94974134bebed257
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-05 18:53:32 +0900

    [Gold V] Title: Contact, Time: 284 ms, Memory: 113560 KB -BaekjoonHub

--- 백준/Gold/1013. Contact/Contact.py
+++ 백준/Gold/1013. Contact/Contact.py
@@ -1,42 +1,10 @@
-import sys
+import re
+n = int(input())
 
-test = int(input())
-for _ in range(test):
-    sig = sys.stdin.readline().rstrip()
-    length =len(sig)
-    flag = True
-
-    i = 0
-    while i < length:
-        if sig[i]=="0" and i+1 < length:
-            if sig[i+1] != "1":
-                flag = False
-                break
-            else:
-                i+=2
-        elif sig[i]=="0" and i+1 >= length:
-            flag = False
-            break
-        elif sig[i]=="1":
-            num0 = 0
-            i+=1
-            while i < length and sig[i]=="0":
-                num0+=1
-                i+=1
-            if num0 <2 :
-                flag=False
-                break
-            num1 = 0
-            while i < length and sig[i]=="1":
-                num1+=1
-                i+=1
-            if i+1<length and sig[i] =="0" and sig[i+1] =="0":
-                num1-=1
-                i-=1
-            if num1 <1 :
-                flag=False
-                break
-    if flag==False:
-        print("NO")
-    else:
+pattern = re.compile("(100+1+|01)+")
+for _ in range(n):
+    line = input()
+    if pattern.fullmatch(line):
         print("YES")
+    else:
+        print("NO")
