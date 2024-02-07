4f6f03b031233e10d9943110742e8e6d9ba76ecb
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-07 22:45:35 +0900

    [level 3] Title: 순위, Time: 93.28 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -0,0 +1,21 @@
+def solution(n, results):
+    graph = [[False for _ in range(n+1)] for _ in range(n+1)]
+    for a,b in results:
+        graph[a][b] = True
+        
+    for k in range(1,n+1):
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if graph[i][k] and graph[k][j]:
+                    graph[i][j] = True
+                    
+    answer = 0
+    for i in range(1,n+1):
+        count = 0
+        for j in range(1,n+1):
+            if graph[i][j] or graph[j][i]:
+                count+=1
+        if count == n-1:
+            answer+=1
+    
+    return answer
