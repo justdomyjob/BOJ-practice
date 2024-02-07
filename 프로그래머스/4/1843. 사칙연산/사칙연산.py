5ccc08f19d2a796e3f3f9b50c632722e1cc8023e
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-07 14:37:14 +0900

    [level 4] Title: 사칙연산, Time: 120.62 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.py
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -1,24 +1,27 @@
 def solution(arr):
     numbers = [int(i) for i in arr if i.isdigit()]
     operations = [i for i in arr if not i.isdigit()]
-    # print(numbers)
-    # print(operations)
     length = len(numbers)
     dp = [[[] for _ in range(length)] for _ in range(length)] #dp[0][2] : 0번째에서 2번째까지 계산한 값
     for i in range(length):
         dp[i][i].append(numbers[i])
+        dp[i][i].append(numbers[i])
     for i in range(1,length):
         for j in range(length-i):
-            for k in range(j,j+i):  #dp[j][j+i] = dp[j][k] * dp[k+1][j+i]
-                for value1 in dp[j][k]:
-                    for value2 in dp[k+1][j+i]:
-                        if operations[k] == "-":
-                            dp[j][j+i].append(value1-value2)
-                        else:
-                            dp[j][j+i].append(value1+value2)
+            for k in range(j,j+i):  
+                m1,M1 = dp[j][k]
+                m2,M2 = dp[k+1][j+i]
+                if operations[k] == "-":
+                    dp[j][j+i].append(m1-m2)
+                    dp[j][j+i].append(m1-M2)
+                    dp[j][j+i].append(M1-m2)
+                    dp[j][j+i].append(M1-M2)
+                else:
+                    dp[j][j+i].append(m1+m2)
+                    dp[j][j+i].append(m1+M2)
+                    dp[j][j+i].append(M1+m2)
+                    dp[j][j+i].append(M1+M2)
             m = min(dp[j][j+i])
             M = max(dp[j][j+i])
-            dp[j][j+i] = [m, M]
-    # for d in dp:
-    #     print(*d)
+            dp[j][j+i] = [m,M]
     return max(dp[0][length-1])
