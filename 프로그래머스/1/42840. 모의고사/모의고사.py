5c0fdbe88cb2345b1357cd257c0bdf2a520e964a
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-07 23:10:04 +0900

    [level 1] Title: 모의고사, Time: 4.33 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,18 @@
+def solution(answers):
+    s1 = [1, 2, 3, 4, 5]
+    s2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    c=[0,0,0]
+    for i in range(len(answers)):
+        if answers[i] ==  s1[i%5]:
+            c[0]+=1
+        if answers[i] ==  s2[i%8]:
+            c[1]+=1
+        if answers[i] ==  s3[i%10]:
+            c[2]+=1
+    m = max(c)
+    answer = []
+    for i,cc in enumerate(c):
+        if cc==m:
+            answer.append(i+1)
+    return answer
