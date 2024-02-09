4ad657be00ecf1c99242b4c96520176b150b8369
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-09 19:26:58 +0900

    [level 3] Title: 네트워크, Time: 0.45 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,20 +1,21 @@
-from collections import deque
 def solution(n, computers):
-    visited = [-1 for _ in range(n)]
+    answer = 0
+    visited = [0 for i in range(n)]
     
-    def bfs(start):
-        visited[start] = 1
-        q = deque()
-        q.append(start)
-        while q:
-            v  = q.pop()
-            for i in range(n):
-                if computers[v][i]==1 and visited[i]==-1:
-                    visited[i] = 1
-                    q.append(i)
-    count = 0
-    for i in range(n):
-        if visited[i]==-1:
-            bfs(i)
-            count+=1
-    return count
+    def dfs(computers, visited, start):
+        stack = [start]
+        while stack:
+            j = stack.pop()
+            if visited[j] == 0:
+                visited[j] = 1
+            for i in range(len(computers)):
+                if computers[j][i] ==1 and visited[i] == 0:
+                    stack.append(i)
+                    
+    i = 0
+    while 0 in visited:
+        if visited[i] ==0:
+            dfs(computers, visited, i)
+            answer += 1
+        i += 1
+    return answer
