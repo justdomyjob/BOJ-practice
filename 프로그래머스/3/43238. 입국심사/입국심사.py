6a7a74461c24e24f595347b9ccc506e8bbaf6bb0
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-12 14:42:13 +0900

    [level 3] Title: 입국심사, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,21 +1,3 @@
 def solution(n, times):
-    def check(minutes):
-        count = 0
-        for time in times:
-            count += minutes//time
-        if count >=n:
-            return True
-        else:
-             return False
-    low = 0
-    high = max(times)*n+1
-    while low+1<high:
-        mid = (low+high)//2
-        if check(mid):
-            high = mid
-        else:
-            low = mid
-    return high
-    
-
-
+    answer = 0
+    return answer
