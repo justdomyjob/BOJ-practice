80c1e53730c9b199270f2d33237e3b07ceff5fdb
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-12 14:30:15 +0900

    [Gold IV] Title: 공유기 설치, Time: 212 ms, Memory: 152632 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,35 +1,31 @@
-try : standard_input = open("input.txt", "r")  
-except:pass
+import sys
 
-home_n, wifi = map(int,input().split())
-home_list = [int(input()) for _ in range(home_n)]
-home_list.sort()
+N,C = map(int,input().split())
+homes = [int(sys.stdin.readline().rstrip()) for _ in range(N)]
+homes.sort()
 
-def can_install(length):
-    install_house_n = 1 #설치한 집 개수 (0번째 집은 미리 설치)
-    before_install_house =0 # 이전에 설치한 집
-    now = 1 #현재 집 
-    #now_x =0 #현재 x좌표
-    while now <= home_n-1 and install_house_n < wifi :
-        if home_list[now] - home_list[before_install_house] >= length:
-            before_install_house = now
-            now = now+1
-            install_house_n +=1
-        else :
-            now = now +1
-    if install_house_n ==wifi:
-        return True
-    else:
+def check(n):
+    before = homes[0]
+    end = homes[-1]
+    count =2
+    for home in homes[1:-1]:
+        if count == C:
+            break
+        if home -before>=n:
+            before = home
+            count+=1
+    if end - before < n:
         return False
+    if count < C:
+        return False
+    return True
 
-left = 1
-#right = max(home_list)-min(home_list) +1 #을 하는 이유? 알아보자 right가 답이 될때가 있어서
-right = (max(home_list)-min(home_list))//(wifi-1) +1
-mid = (left+right)//2
-while left +1 < right:
-    if can_install(mid):
-        left = mid
-    else :
-        right = mid
-    mid = (left+right)//2 
-print(mid)
+low = 1
+end = max(homes)+1
+while low + 1 < end:
+    mid = (low+end)//2
+    if check(mid):
+        low = mid
+    else:
+        end = mid
+print(low)
