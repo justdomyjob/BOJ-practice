bee6b34c5b0a11d6b9d9fff33e5d71888adeacfc
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-02-12 13:18:14 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9620 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,18 +1,10 @@
-import sys
+n = int(input())
 
-n = int(sys.stdin.readline().rstrip())
-#A = [int(sys.stdin.readline().rstrip()) for _ in range(n)]
-F = [0 for _ in range(10001)]
+import sys
+dic = [0] * 10001
 for _ in range(n):
     a = int(sys.stdin.readline().rstrip())
-    F[a]+=1
+    dic[a] += 1
 for i in range(1,10001):
-    F[i] = F[i]+F[i-1]
-before =0 
-for i in range(1,10001):
-    if F[i]==before:
-        continue
-    else:
-        for j in range(F[i]-before):
-            print(i)
-        before = F[i]
+    for j in range(dic[i]):
+        print(i)
