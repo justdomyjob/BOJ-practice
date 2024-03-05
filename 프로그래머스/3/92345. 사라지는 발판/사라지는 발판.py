6196382d29f9e9ab425220c5f0fa9085ada46b40
justdomyjob <152833253+justdomyjob@users.noreply.github.com>
2024-03-05 23:01:04 +0900

    [level 3] Title: 사라지는 발판, Time: 0.14 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
@@ -6,12 +6,9 @@ def solution(board, aloc, bloc):
     r = len(board)
     c = len(board[0])
     def A_win(board,aloc,bloc): #A가 이기면 True이면서 가장 작은 값 수 지면 False 이면서 가장 큰 값
-
-        #초기값
         y,x = aloc
         if board[y][x] == 0:
             return False,0
-
         ret = [] #모든 값이 -1이면 -1 리턴 하나라도 있으면 그 수 +1
         for k in range(4):
             ny = y + dy[k]
@@ -37,9 +34,4 @@ def solution(board, aloc, bloc):
                 numbers = [i[1] for i in ret]
                 count = max(numbers)
                 return False,count+1
-    print(A_win(board,aloc,bloc))
     return A_win(board,aloc,bloc)[1]
-# solution([[1, 1, 1], [1, 1, 1], [1, 1, 1]],[1,0],[1,2])
-# solution([[1, 1, 1], [0, 0, 0], [1, 1, 1]],[0,1],[0,1])
-# solution([[1, 1, 1, 1, 1]],[0,0],[0,4])
-# solution(	[[1, 1, 1], [0, 0, 1], [1, 1, 1]], [1, 2], [0, 0])
